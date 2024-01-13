6be25c8530b18c10b0b61205a09c44b8d43cede7
Vivek Kumar <vickyvivek557@gmail.com>
2024-01-13 16:56:46 +0530

    completed

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr
+  .filter((employee) => employee.profession === "developer")
+  .map((developer) => console.log(developer));
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((employee) => {
+    if(employee.profession === "developer"){
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(newEmployee);
 }
 
 function removeAdmin() {
-  //Write your code here, just console.log
+  const filteredArr = arr.filter((employee) => {
+   return employee.profession !== "admin"
+  });
+  console.log(filteredArr);
 }
+
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArray = [
+    { id: 4, name: "alice", age: "22", profession: "designer" },
+    { id: 5, name: "bob", age: "25", profession: "analyst" },
+    { id: 6, name: "dave", age: "28", profession: "manager" },
+  ];
+
+  const concatenatedArray = arr.concat(newArray);
+  console.log(concatenatedArray);
 }
